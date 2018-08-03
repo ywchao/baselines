e19e29fdc6f038bd7050be644fb110363cb9580a
ywchao <ywchao@umich.edu>
2018-08-03 03:47:51 -0400

    Add RoboschoolHumanoidBullet3

---
+++ scripts/trpo_mpi_RoboschoolHumanoidBullet3-v1.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+if [ $# -eq 0 ]; then
+  seed=0
+else
+  seed=$1
+fi
+
+time python -m baselines.trpo_mpi.run_mujoco \
+  --env RoboschoolHumanoidBullet3-v1 \
+  --seed $seed \
+  --num-timesteps 40000000
+
+OUT_DIR="output/trpo_mpi/RoboschoolHumanoidBullet3-v1.seed_$seed.num-timesteps_4.00e+07"
+
+time python -m baselines.trpo_mpi.vis_model \
+  --env RoboschoolHumanoidBullet3-v1 \
+  --seed 0 \
+  --load-model-path $OUT_DIR/checkpoints/model.ckpt-39000 \
+  --render-mode array \
+  --vis-file $OUT_DIR/vis_model.ckpt-39000.mp4
