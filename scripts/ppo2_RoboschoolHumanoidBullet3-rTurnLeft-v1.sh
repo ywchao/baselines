97838d5b2aec2ad9297ae3e663d8d8e078ec15ef
ywchao <ywchao@umich.edu>
2018-10-28 15:59:29 -0400

    Rename Turn to TurnLeft + add RoboschoolHumanoidBullet3-rTurnRight

--- scripts/ppo2_RoboschoolHumanoidBullet3-rTurnLeft-v1.sh
+++ scripts/ppo2_RoboschoolHumanoidBullet3-rTurnLeft-v1.sh
@@ -9,19 +9,18 @@ else
 fi
 
 time python -m baselines.ppo2.run_roboschool \
-  --env=RoboschoolHumanoidBullet3-rTurn-train-v1 \
+  --env=RoboschoolHumanoidBullet3-rTurnLeft-train-v1 \
   --seed=$seed \
   --num-timesteps=30000000 \
   --nsteps=8192 \
   --nminibatches=32 \
   --noptepochs=4 \
-  --lr=1e-4 \
-  --load-model-path=output/ppo2/RoboschoolHumanoidBullet3-rWalk-train-v1.seed_0.num-timesteps_8.00e+07/checkpoints/09700
+  --lr=1e-4
 
-OUT_DIR="output/ppo2/RoboschoolHumanoidBullet3-rTurn-train-v1.seed_$seed.num-timesteps_3.00e+07"
+OUT_DIR="output/ppo2/RoboschoolHumanoidBullet3-rTurnLeft-train-v1.seed_$seed.num-timesteps_3.00e+07"
 
 time python -m baselines.ppo2.vis_model \
-  --env=RoboschoolHumanoidBullet3-rTurn-v1 \
+  --env=RoboschoolHumanoidBullet3-rTurnLeft-v1 \
   --seed=0 \
   --load-model-path=$OUT_DIR/checkpoints/03600 \
   --render-mode=array \
